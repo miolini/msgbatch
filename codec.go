3114f094c547294dfbdcf13994d286a6cd9b06a4
Artem Andreenko <miolini@gmail.com>
2014-07-17 05:45:55 +0400

    first commit

---
+++ codec.go
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright 2014 by Artem Andreenko, Openstat.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+ package msgbatch
+
+import (
+	"code.google.com/p/snappy-go/snappy"
+	"github.com/vmihailenco/msgpack"
+)
+
+type Batch struct {
+	Length  int
+	Columns []string
+	Values  [][]string
+}
+
+func (batch *Batch) Add(e map[string]string) {
+	for key, value := range e {
+		columnIndex := -1
+		for i, column := range batch.Columns {
+			if column == key {
+				columnIndex = i
+				break
+			}
+		}
+		if columnIndex == -1 {
+			columnIndex = len(batch.Columns)
+			batch.Columns = append(batch.Columns, key)
+			batch.Values = append(batch.Values, []string{})
+		}
+		batch.Values[columnIndex] = append(batch.Values[columnIndex], value)
+	}
+	batch.Length++
+}
+
+func (batch *Batch) GetValues() (values []map[string]string) {
+	for i := 0; i < batch.Length; i++ {
+		values = append(values, make(map[string]string))
+	}
+	for columnIndex, column := range batch.Values {
+		columnName := batch.Columns[columnIndex]
+		for valueIndex, value := range column {
+			values[valueIndex][columnName] = value
+		}
+	}
+	return
+}
+
+func (batch *Batch) Encode() (data []byte, err error) {
+	data, err = msgpack.Marshal(batch)
+	if err != nil {
+		return
+	}
+	data, err = snappy.Encode(nil, data)
+	return
+}
+
+func Decode(data []byte) (batch Batch, err error) {
+	data, err = snappy.Decode(nil, data)
+	if err != nil {
+		return
+	}
+	err = msgpack.Unmarshal(data, &batch)
+	return
+}
---
+++ codec_test.go
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright 2014 by Artem Andreenko, Openstat.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+ package msgbatch
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestEncode(t *testing.T) {
+	batch := genBatch(10)
+	data, err := batch.Encode()
+	if err != nil {
+		t.Logf("encode err: %s", err)
+	}
+	t.Logf("data: %v", data)
+}
+
+func BenchmarkEncode(b *testing.B) {
+	batch := genBatch(1000)
+	var data []byte
+	var err error
+	for i := 0; i < b.N; i++ {
+		data, err = batch.Encode()
+	}
+	if err != nil {
+		b.Logf("encode err: %s", err)
+	} else {
+		b.Logf("data %d", len(data))
+	}
+}
+
+func BenchmarkDecode(b *testing.B) {
+	batch := genBatch(1000)
+	data, err := batch.Encode()
+	for i := 0; i < b.N; i++ {
+		batch, err = Decode(data)
+	}
+	if err != nil {
+		b.Logf("encode err: %s", err)
+	} else {
+		b.Logf("batch length: %d", len(batch.GetValues()))
+	}
+}
+
+func genBatch(num int) Batch {
+	batch := Batch{}
+	for i := 0; i < 1000; i++ {
+		e := map[string]string{}
+		e["ts"] = fmt.Sprintf("%d", time.Now().Unix())
+		e["url"] = fmt.Sprintf("http://%d/%d", rand.Int(), rand.Int())
+		e["rf"] = fmt.Sprintf("http://%d/%d", rand.Int(), rand.Int())
+		e["rand"] = fmt.Sprintf("%d", rand.Int())
+		batch.Add(e)
+	}
+	return batch
+}
