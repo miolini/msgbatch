fd03734e2ad204592a1932bdc9c9bfda3a6cb59c
Artem Andreenko <miolini@gmail.com>
2014-08-24 13:56:26 +0700

    Update codec.go

--- codec.go
+++ codec.go
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2014 by Artem Andreenko, Openstat.
+ * Copyright 2014 by Artem Andreenko.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
